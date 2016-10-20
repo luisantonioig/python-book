4aa638b4a0155760a09e8e50ab9539673f4c441a
luisantonioig <c-remix@hotmail.es>
2016-10-20 12:06:37 -0500

    empeze lo de listas

---
+++ 2. Working with data/list.py
@@ -0,0 +1,19 @@
+a = [1,2]
+b=[1.5,1,a]
+print b
+
+print range(5)
+print range(3,8)
+print range(0,10,2)
+
+print len(range(0,10,2))
+
+a = [1,2]
+b = [3,4,5,6]
+print a + b
+print a * 2
+x = [1, 2, 3, 4]
+print x
+print x[-1]
+print x[-2]
+print x[0:2]
